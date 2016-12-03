ccdae346d8531d82e65ea18b5136f25eb359752d
Master3395 <master3395@users.noreply.github.com>
2016-12-03 17:00:03 +0100

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 Plugin Name: Fallback to main website
 Plugin URI: https://roblox-network.com/yourls-plugins
 Description: Users can change their url in the plugin file.
-Version: 1.0
+Version: 1.2
 Author: Master3395 <info@roblox-network.com>
 Author URI: https://roblox-network.com/master3395
 */
