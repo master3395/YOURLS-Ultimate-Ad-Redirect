aa808e2f5d0bb10a473ad9bd046ae8daa88d03d2
Master3395 <master3395@users.noreply.github.com>
2018-04-08 02:46:47 +0200

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -1,10 +1,10 @@
 <?php
 /*
 Plugin Name: Fallback to main website
-Plugin URI: https://newstargeted.com/yourls-plugins
+Plugin URI: https://newstargeted.com/index.php/misc/yourls/yourls-plugins
 Description: Users can change their url in the plugin file.
-Version: 1.3
-Author: Master3395 <info@roblox-network.com>
+Version: 1.4
+Author: Master3395 <info@newstargeted.com>
 Author URI: https://newstargeted.com/master3395
 */
 
